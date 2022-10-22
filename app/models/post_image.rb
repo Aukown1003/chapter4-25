1aaa42e781b2bfdd3f8a8bac2160838e3fa57077
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-10-22 08:47:10 +0000

    [Start]4-11

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,15 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  
+  # 呼び出したい処理名
+  def get_image
+    # もしimageが添付されていなければno_image.jpgを呼び出す
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    else
+      image
+    end
+  end
 end
