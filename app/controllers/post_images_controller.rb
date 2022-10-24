5d898320be29cfef6576ddf5c7753b87862a1577
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-10-24 04:35:08 +0000

    [Finish]4-12

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -16,7 +16,7 @@ class PostImagesController < ApplicationController
   end
 
   def show
-    @post_imege = PostImage.find(params[:id])
+    @post_image = PostImage.find(params[:id])
   end
   
   private
