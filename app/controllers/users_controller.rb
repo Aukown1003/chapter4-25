672fd3475a360bf58c6431358dc570613d551b59
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-10-26 09:14:35 +0000

    [Finish]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  # "edit"と"update"のアクションの実行前に、"is_matching_login_user"を実行
+   before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     # @post_images = @user(上で特定したuser)の関連付けられた投稿を全て取得
@@ -9,15 +12,23 @@ class UsersController < ApplicationController
   end
 
   def edit
-    user_id = params[:id].to_i
-    login_user_id = current_user.id
-    if(user_id != login_user_id)
-      redirect_to post_imeges_path
-    end
+    # ユーザーID　＝　編集しようとした場所のID
+    # ログインユーザーID　＝　現在ログインしているID
+    # 両者が不一致の時一覧にリダイレクト
+    # user_id = params[:id].to_i
+    # login_user_id = current_user.id
+    # if(user_id != login_user_id)
+    #   redirect_to post_imeges_path
+    # end
     @user = User.find(params[:id])
   end
 
   def update
+    # user_id = params[:id].to_i
+    # login_user_id = current_user.id
+    # if(user_id != login_user_id)
+    #   redirect_to post_images_path
+    # end
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
@@ -28,4 +39,15 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name,:profile_image)
   end
+end
+
+
+# CreateとUpdateのコードは一部同じ＝纏める事ができる
+# 一番上のbefore_actionでedit,updateの最初に実行する
+def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to post_images_path
+    end
 end
